c296dfe4ebd9f2f513d708f6bb75c6603168846d
MannanShaikh <arsalaanshaikh8@gmail.com>
2023-11-28 23:01:20 +0530

    News

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Exceptions\PageNotFoundException;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function view($page = 'home')
+    {
+        // ...
+    }
+}
